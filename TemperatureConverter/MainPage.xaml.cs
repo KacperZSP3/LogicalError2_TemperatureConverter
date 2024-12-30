92ac613f2854eb5a10aaa258875310a553d76dad
KacperZSP3 <k.ksykiewicz@zsp3zamosc.pl>
2024-12-30 11:10:34 +0100

    LogicalErroe2_TemperatureConverter with instructions

---
+++ TemperatureConverter/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace TemperatureConverter;
+
+//Temperatura jest 
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnConvertClicked(object sender, EventArgs e)
+    {
+        if (double.TryParse(CelsiusEntry.Text, out double celsius))
+        {
+            double fahrenheit = (celsius * 5 / 9) + 32;
+            ResultLabel.Text = "Fahrenheit: " + fahrenheit;
+        }
+        else
+        {
+            ResultLabel.Text = "Invalid input";
+        }
+    }
+}
